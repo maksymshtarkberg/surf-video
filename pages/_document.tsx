a965402457662707c49db5d1e7418cc57a6f1bf2
maksymshtarkberg <starkberg@icloud.com>
2024-09-12 01:54:50 +0300

    google ads add

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,11 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap"
             rel="stylesheet"
           />
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3690002536030103"
+            crossOrigin="anonymous"
+          ></script>
         </Head>
         <body>
           <Main />
